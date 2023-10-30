1e72ff31e76441d87bf22b94c399a6035797d146
snusEbjoer <pojiratelsnusa@gmail.com>
2023-10-30 16:40:46 +0300

    fixed

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -14,11 +14,9 @@ export type TodoItemProps = {
 
 export const TodoItem: React.FC<TodoItemProps> = ({ todo, deleteToDo, changeDone, moveUp, moveDown, select, changeSelected }) => {
 	const [isShown, setIsShown] = useState<boolean>(false)
-	const toggleIsShown = () => {
-		setIsShown(prev => !prev)
-	}
+	
 	return (
-		<div key={todo.id} className="inline-flex justify-end items-center max-w-lg gap-3 w-full min-w-xs text-md p-3.5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" onMouseEnter={toggleIsShown} onMouseLeave={toggleIsShown}>
+		<div key={todo.id} className="inline-flex justify-end items-center max-w-lg gap-3 w-full min-w-xs text-md p-3.5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
 			<div className="flex justify-between w-full items-center">
 			<p className={todo.done === true ? 'text-black opacity-50 line-through' : ''}>{todo.name}</p>
 			<div className="flex flex-row gap-1 w-4 h-4 items-center justify-end">
@@ -48,7 +46,7 @@ export const TodoItem: React.FC<TodoItemProps> = ({ todo, deleteToDo, changeDone
 </button>
 			</div>)
 			}
-			{select === true ? 
+			{select === true && todo.done === false ? 
 				<input className="p-3 w-3 h-3 outline-none text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-0 dark:bg-gray-700 dark:border-gray-600" onChange={() => changeSelected(todo.id)} type="checkbox"/> 
 			: <div></div>}
 			</div>
